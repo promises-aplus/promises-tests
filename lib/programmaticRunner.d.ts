24c0a8b76ad13a27fa156e40463b7698798d4f0b
Shad Sterling <me@shadsterling.com>
2022-11-18 21:18:15 -0500

    Replace tshint with TypeScript, replace handwritten type declarations with generated

--- lib/programmaticRunner.d.ts
+++ lib/programmaticRunner.d.ts
@@ -1,27 +1,28 @@
-// Type definitions for Promises/A+ Compliance Test Suite - https://github.com/promises-aplus/promises-tests
-// Definitions by Shad Sterling - https://github.com/ShadSterling
-
-declare module "promises-aplus-tests" {
-
-	export = promisesAPlusTests;
-
-	function promisesAPlusTests( implementation: promisesAPlusTests.Adapter<any>, callback: promisesAPlusTests.Callback ): void
-	function promisesAPlusTests( implementation: promisesAPlusTests.Adapter<any>, mochaOptions: any, callback: promisesAPlusTests.Callback ): void
-
-	namespace promisesAPlusTests {
-		export type Callback = ( error_count: number ) => void;
-		export type Tester = typeof promisesAPlusTests;
-		export type Adapter<T=any> = {
-			resolved?: (  value: T   ) => Promise<T>,
-			rejected?: ( reason: any ) => Promise<never>,
-			deferred: <U=T>() => Deferred<U>;
-		}
-		export type Deferred<T=any> = {
-			promise: Promise<T>,
-			resolve: (  value: T   ) => void,
-			reject:  ( reason: any ) => void,
-		}
-		export function mocha( adapter: Adapter<any> ): void;
-	}
-
+export = promisesAPlusTests;
+/** @type { PromisesAPlusTests.Export } */
+declare var promisesAPlusTests: PromisesAPlusTests.Export;
+declare namespace PromisesAPlusTests {
+    type Adapter<T> = {
+        resolved?: (value: T) => Promise<T>;
+        rejected?: (reason: any) => Promise<never>;
+        deferred: <U = T>() => PromisesAPlusTests.Deferred<U>;
+    };
+    type Callback = (error?: PromisesAPlusTests.TestError) => void;
+    type Deferred<T> = {
+        promise: Promise<T>;
+        resolve: (value: T) => void;
+        reject: (reason: any) => void;
+    };
+    type Export = (PromisesAPlusTests.TesterWithOptions | PromisesAPlusTests.TesterWithoutOptions) & PromisesAPlusTests.MochaTester;
+    type MaybeFailureCount = {
+        failure_count?: number;
+    };
+    type MochaTester = {
+        mocha: PromisesAPlusTests.MochaTestMethod;
+    };
+    type MochaTestMethod = (impormentation: PromisesAPlusTests.Adapter<any>) => void;
+    type Tester = PromisesAPlusTests.TesterWithOptions & PromisesAPlusTests.TesterWithoutOptions;
+    type TestError = Error & PromisesAPlusTests.MaybeFailureCount;
+    type TesterWithOptions = (implementation: PromisesAPlusTests.Adapter<any>, mochaOptions: any, callback?: PromisesAPlusTests.Callback) => void;
+    type TesterWithoutOptions = (implementation: PromisesAPlusTests.Adapter<any>, callback?: PromisesAPlusTests.Callback) => void;
 }
